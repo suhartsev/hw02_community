65896313ab0892125a766a3964adda4bb5222ba6
Sergey Sukhartsev <bloxazavr@mail.ru>
2022-12-13 21:47:13 +0300

    tests PASSED

---
+++ yatube/posts/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Post, Group
+
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    template = 'posts/index.html'
+    context = {
+        'posts': posts,
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)

02e53009932b40b372fbf9ef542b21d457b3acce
Sergey Sukhartsev <bloxazavr@mail.ru>
2022-12-15 10:44:17 +0300

    pop8

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#константа для лимита вывода постов на страницу(константа для views.py)
+# константа для лимита вывода постов на страницу(константа для views.py)
 LIMIT_POSTS = 10
 
 # Quick-start development settings - unsuitable for production

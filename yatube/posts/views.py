ad136c27ded87378949658ed725958ea7666eb41
Sergey Sukhartsev <bloxazavr@mail.ru>
2022-12-15 19:29:27 +0300

    setting import

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import get_object_or_404, render
 
-from yatube.settings import LIMIT_POSTS
+from django.conf import settings
 
 from .models import Post, Group
 
 
 def index(request):
-    posts = Post.objects.all()[:LIMIT_POSTS]
+    posts = Post.objects.all()[:settings.LIMIT_POSTS]
     context = {
         'posts': posts,
     }
@@ -15,7 +15,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:LIMIT_POSTS]
+    posts = group.posts.all()[:settings.LIMIT_POSTS]
     context = {
         'group': group,
         'posts': posts,

780b7f9c6b1c486cb57690cfaf96e4eff92045a9
Sergey Sukhartsev <bloxazavr@mail.ru>
2022-12-16 15:58:41 +0300

    попытка №3

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -33,3 +33,6 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404, render
-
 from django.conf import settings
 
 from .models import Post, Group
@@ -7,17 +6,12 @@ from .models import Post, Group
 
 def index(request):
     posts = Post.objects.all()[:settings.LIMIT_POSTS]
-    context = {
-        'posts': posts,
-    }
-    return render(request, 'posts/index.html', context)
+    return render(request, 'posts/index.html', {'posts': posts})
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all()[:settings.LIMIT_POSTS]
-    context = {
-        'group': group,
-        'posts': posts,
-    }
-    return render(request, 'posts/group_list.html', context)
+    return render(request, 'posts/group_list.html',
+    {'group': group,
+     'posts': posts,})
